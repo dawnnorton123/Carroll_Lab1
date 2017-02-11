5cdf52566171e9676d293557b37daacf4e87b848
“dawn <dnorton@carroll.edu>
2017-02-11 10:34:41 -0700

    add new UI to create Tasks

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,16 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("hello, world"));
+        return ok(index.render("HELLO, WORLD"));
     }
 
+    public static Result addTask() {
+        play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
+        models.Task task = form.get();
+        task.save();
+        return redirect(routes.Application.index());
+    }
+    
+    
+    
 }
