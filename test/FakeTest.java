0c48e07e8672d4dbecf97b0a4850c9e25cce0977
“dawn <user@git.org>
2017-03-14 09:15:10 -0600

    test/FakeTest.java

--- test/FakeTest.java
+++ test/FakeTest.java
@@ -1,9 +1,10 @@
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-
-class FakeTest {
-    @Test
-    public void fakeTest() {
-        assertTrue(true);
-    }
-}
+//import static org.junit.Assert.assertTrue;
+//
+//import org.junit.Test;
+//
+//class FakeTest {
+//    @Test
+//    public void fakeTest() {
+//        assertTrue("Who....., true is not true",true);
+//    }
+//}	
