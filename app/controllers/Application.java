c7de833f665075b27cceaf8d96e9b0279197dda6
“dawn <user@git.org>
2017-04-18 11:09:45 -0600

    app

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,7 @@ import models.TaskForm;
 import services.TaskPersistenceService;
 
 import views.html.index;
+import views.html.dawnsPage;
 
 import play.data.Form;
 import play.mvc.Controller;
@@ -16,20 +17,26 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @Named
 public class Application extends Controller {
+	
+	 private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     @Inject
     private TaskPersistenceService taskPersist;
 
     public Result index() {
-        return ok(index.render("hello, world", Form.form(TaskForm.class)));
+        return ok(index.render("Dawn's Page!!", Form.form(TaskForm.class)));
     }
 
     public Result addTask() {
         Form<TaskForm> form = Form.form(TaskForm.class).bindFromRequest();
         if (form.hasErrors()) {
-            return badRequest(index.render("hello, world", form));
+            return badRequest(index.render("Dawn's Page!!", form));
         }
 
         Task1 task = new Task1();
@@ -42,4 +49,15 @@ public class Application extends Controller {
         List<Task1> tasks = taskPersist.fetchAllTasks();
         return ok(play.libs.Json.toJson(tasks));
     }
+    
+
+    public Result dawnsPage() {
+    	log.warn("Entering Dawns Page!!");
+        return ok(dawnsPage.render("Dawn's Page!!"));
+        
+        
+    }
+
+
+  
 }
--- app/models/TaskForm.java
+++ app/models/TaskForm.java
@@ -1,12 +1,18 @@
 package models;
 
 import play.data.validation.Constraints.Required;
+import play.data.validation.Constraints.MinLength;
+import play.data.validation.Constraints.MaxLength;
+import play.data.validation.Constraints.MinValidator;
 
 
 public class TaskForm {
 
     @Required
+    @MinLength(3)
+    @MaxLength(10)
     private String contents;
+    
 
     public String getContents() {
         return contents;
